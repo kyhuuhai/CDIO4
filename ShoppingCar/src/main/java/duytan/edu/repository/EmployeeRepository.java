1e49578e6a92b9c16bc42ff110f79ca81addedc6
Bin <kyhuuhai@gmail.com>
2016-10-21 21:56:21 +0700

    Login

--- ShoppingCar/src/main/java/duytan/edu/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package duytan.edu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import duytan.edu.entity.EmployeeEntity;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,String>{
-
-}
--- ShoppingCar/src/main/java/duytan/edu/service/EmployeeEntityManager.java
+++
@@ -1,9 +0,0 @@
-package duytan.edu.service;
-
-import java.util.List;
-
-import duytan.edu.entity.EmployeeEntity;
-
-public interface EmployeeEntityManager {
-	List<EmployeeEntity> findAll();
-}
